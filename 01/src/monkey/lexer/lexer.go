8e9395260c8194017f2c53f91a6ccb2a7d287949
jakeadel <jakeadel0@gmail.com>
2024-08-29 00:34:37 -0400

    Lexer done for now

--- 01/src/monkey/lexer/lexer.go
+++ 01/src/monkey/lexer/lexer.go
@@ -1,6 +1,9 @@
 package lexer
 
-import "monkey/token"
+import (
+	"fmt"
+	"monkey/token"
+)
 
 type Lexer struct {
 	input        string
@@ -23,15 +26,39 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition += 1
+}
 
+func (l *Lexer) peekChar() byte {
+	if (l.readPosition >= len(l.input)) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
 }
 
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhitespace()
+	fmt.Println(l.position, string(l.ch))
+
 	switch l.ch {
 	case '=':
-		tok = newToken((token.ASSIGN), l.ch)
+		if l.peekChar() == '=' {
+			l.readChar()
+			tok.Literal = "=="
+			tok.Type = token.EQ
+		} else {
+			tok = newToken((token.ASSIGN), l.ch)
+		}
+	case '!':
+		if l.peekChar() == '=' {
+			l.readChar()
+			tok.Literal = "!="
+			tok.Type = token.NOT_EQ
+		} else {
+			tok = newToken((token.BANG), l.ch)
+		}
 	case ';':
 		tok = newToken((token.SEMICOLON), l.ch)
 	case '(':
@@ -46,9 +73,33 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken((token.COMMA), l.ch)
 	case '+':
 		tok = newToken((token.PLUS), l.ch)
+	case '-':
+		tok = newToken((token.MINUS), l.ch)
+	case '/':
+		tok = newToken((token.SLASH), l.ch)
+	case '*':
+		tok = newToken((token.ASTERISK), l.ch)
+	case '<':
+		tok = newToken((token.LT), l.ch)
+	case '>':
+		tok = newToken((token.GT), l.ch)
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			// Need to read until not a letter to get variable name
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			// Return early because readIdentifier advances our position
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Literal = l.readNumber()
+			tok.Type = token.INT
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 	l.readChar()
 	return tok
@@ -56,4 +107,44 @@ func (l *Lexer) NextToken() token.Token {
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isLetter(letter byte) bool {
+	if (letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z') || letter == '_' {
+		return true
+	} else {
+		return false
+	}
+}
+
+// Advance until we find a non-letter, return string of letters advanced over
+func (l *Lexer) readIdentifier() string {
+	startPosition := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[startPosition:l.position]
+}
+
+func isDigit(char byte) bool {
+	if (char >= '0' && char <= '9') {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (l *Lexer) readNumber() string {
+	startPosition := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[startPosition:l.position]
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		fmt.Println(l.ch)
+		l.readChar()
+	}
 }
--- 01/src/monkey/token/token.go
+++ 01/src/monkey/token/token.go
@@ -11,13 +11,21 @@ const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
 
-	// Identifiers + Literals
+	// User-defined Identifiers + Literals
 	IDENT = "IDENT" // add, foobar, x, y, ...
 	INT   = "INT"   // 1, 2, 3, ...
 
 	// Operators
 	ASSIGN = "="
 	PLUS   = "+"
+	MINUS = "-"
+	SLASH = "/"
+	ASTERISK = "*" 
+	LT = "<"
+	GT = ">"
+	BANG = "!"
+	EQ = "=="
+	NOT_EQ = "!="
 
 	// Delimiters
 	COMMA     = ","
@@ -31,4 +39,32 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
 )
+
+// Define language keywords to distinguish from variable names
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
+	"true": TRUE,
+	"false": FALSE,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
+
+/*
+	Go note:
+	In lookupIdent, tok = value, ok = does value exist in the map, ident = key
+*/
